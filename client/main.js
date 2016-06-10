dc54c2c23f7c0c6cac7d041c26be5779b745b73e
Sebastian Quack <sebastianquack@gmail.com>
2016-06-10 23:22:03 +0200

    player management and moving between rooms

--- client/main.js
+++ client/main.js
@@ -1,17 +1,20 @@
 import { Template } from 'meteor/templating'
 import './main.html'
 
-Template.registerHelper( 'optionsDisplay', () => { return Session.get("formDisplay") ? "hidden" : "" })
+Template.registerHelper( 'manageDisplay', () => { return Session.get("displayMode") == "overview" || Session.get("displayMode") == "edit" ? "" : "hidden" })
+Template.registerHelper( 'overviewDisplay', () => { return Session.get("displayMode") == "overview" ? "" : "hidden" })
+Template.registerHelper( 'playDisplay', () => { return Session.get("displayMode") == "play" ? "" : "hidden" })
 
-Template.rooms.onCreated(function() {
+Template.roomOverview.onCreated(function() {
   Meteor.subscribe('Rooms')
+  Session.set("displayMode", "overview")
 })
 
-Template.rooms.helpers({
+Template.roomOverview.helpers({
   rooms() { return Rooms.find() }  
 })
 
-Template.rooms.events({  
+Template.newRoomForm.events({  
   'submit .new-room'(event) {
     event.preventDefault()
     Meteor.call('rooms.create', event.target.name.value)
@@ -19,70 +22,122 @@ Template.rooms.events({
   }
 })
 
-Template.room.helpers({
+Template.roomDetails.helpers({
   editorOptions() { return {lineNumbers: true, mode: "javascript"} },
-  formDisplay() { return Session.get("formDisplay") == this._id ? "" : "hidden" }
+  editorDisplay() { return Session.get("displayMode") == "edit" && Session.get("activeRoom") == this._id ? "" : "hidden" },
 })
 
-Template.room.rendered = function() {
+Template.roomDetails.rendered = function() {
   this.editor = CodeMirror.fromTextArea(this.find(".room-script"), {
     lineNumbers: true,
   	mode: "javascript"
   });
 }
   
-Template.room.events({
+Template.roomDetails.events({
+  'click .start-play-button'(event, template) {
+    //move player to selected room
+    roomAPI.movePlayerToRoom(this.name)
+    Session.set("displayMode", "play")
+  },
   'click .open-form-button'(event, template) {
-    Session.set("formDisplay", this._id)
+    Session.set("displayMode", "edit")
+    Session.set("activeRoom", this._id)
     $(template.find(".test-log")).html("")
     template.find(".test-input").value = ""
     template.find(".room-script").value = this.script
     Meteor.setTimeout(function() {
       template.editor.refresh()
     }, 100)
-    
   },
-  
-  'click .cancel-button'(event, template) {
-    Session.set("formDisplay", undefined)
+  'click .cancel-edit-button'(event, template) {
+    Session.set("displayMode", "overview")
   },
-
   'click .save-script-button'(event, template) {
     Meteor.call('rooms.updateScript', this._id, template.editor.getValue())
-    Session.set("formDisplay", undefined)
+    Session.set("displayMode", "overview")
   },
-  
-  'submit .test-room'(event, template) {
+  'submit .test-form'(event, template) {
     event.preventDefault()
     var input = template.find(".test-input").value    
-    template.find(".test-input").value = ""
     var testLog = $(template.find(".test-log"))
-    testLog.append("<li>input: " + input + "</li>")
-    testLog.scrollTop(testLog[0].scrollHeight)
-    
-    // setup untrusted code to be processed as jailed plugin
-    var code = 
-      "var api = {processInput: function(input, output) {" 
-      + template.editor.getValue() +
-      "}}; application.setInterface(api);"
-
-    // create plugin
-    var plugin = new jailed.DynamicPlugin(code)
-    
-    // called after the plugin is loaded
-    plugin.whenConnected(function() {
-      plugin.remote.processInput(input, function(output) {
-        testLog.append("<li>output: " + output + "</li>")
-        testLog.scrollTop(testLog[0].scrollHeight)
-      })    
-    })
+    processInput(input, template.editor.getValue(), testLog)
+    template.find(".test-input").value = ""
   },
-
   'click .remove-room-button'(event) {
     if(confirm("permanently remove room?")) {
       Meteor.call('rooms.remove', this._id)
     }
   }
 })
+
+Template.play.events({
+  'click .cancel-play-button'(event, template) {
+    Session.set("displayMode", "overview")
+  },
+  'submit .play-form'(event, template) {
+    event.preventDefault()
+    var input = template.find(".play-input").value    
+    var playLog = $(template.find(".play-log"))
+    var roomScript = Rooms.findOne({name: Meteor.user().profile.currentRoom}).script
+    processInput(input, roomScript, playLog)    
+    template.find(".play-input").value = ""
+  }
+})
+
+// this is exposed to the plugin script in the rooms - called by application.remote.function
+roomAPI = { 
+  movePlayerToRoom: function(roomName) {
+    var playLog = $(".play-log")
+    if(Rooms.findOne({name: roomName})) {
+      Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.currentRoom": roomName}});
+      logAction("[you are now in room " + roomName + "]", playLog)
+    } else {
+      logAction("[room " + roomName + " not found]", playLog)
+    }
+  }
+}
+
+processInput = function(input, roomScript, log) {  
+  logAction("input: " + input, log)
+  
+  // setup untrusted code to be processed as jailed plugin
+  var pluginCode = 
+      "var api = {" 
+      + "processInput: function(input, output) {" // name the callback function output
+      + roomScript
+      + "}};"
+      + "application.setInterface(api);"
+
+  // create plugin
+  var plugin = new jailed.DynamicPlugin(pluginCode, roomAPI)
+  var response = false
+  
+  // called after the plugin is loaded
+  plugin.whenConnected(function() {
+    // run the process function on the sandboxed plugin
+    plugin.remote.processInput(input, function(outputValue) { 
+      logAction(outputValue, log)
+      response = true
+      plugin = null      
+    })  
+  })
+  
+  Meteor.setTimeout(function() {
+    if(plugin) {
+      plugin.disconnect()
+      plugin = null
+    }
+    if(!response) {
+     logAction("[there was no response]", log) 
+    }
+  }, 1000)
+  
+}
+
+logAction = function(text, log) {
+  log.append("<li>"+ text + "</li>")
+  log.scrollTop(log[0].scrollHeight)
+}
 
 
--- server/main.js
+++ server/main.js
@@ -1,5 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // code to run on server at startup  
+  AccountsGuest.anonymous = true
+  
 })
+
+// default player setup
+Accounts.onCreateUser(function(options, user) {
+  // we keep the default hook's 'profile' behavior.
+  if (options.profile) {
+    user.profile = options.profile;
+  }
+  
+  // todo: add initial settings of user.profile here
+  user.profile.currentRoom = null
+
+  return user;
+});
+
